c89021a42cc5917d3b7f015d62e8b77fae3f9ba3
Alan Almeida <alanalmeida.emp@gmail.com>
2021-06-22 12:40:00 -0300

    installed ts-node-dev | created basic server.ts

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.post('/test', (request, response) => {
+  return response.send('Ola nlw')
+})
+
+app.listen(3333, () => console.log('Server is running!'))
